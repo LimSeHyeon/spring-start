443df9157378d127241bbc55d0822baeb208024e
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-18 09:42:50 +0900

    feat: product 컨트롤러 간단하게 만들어보기

--- src/main/java/com/example/summer/trip/TripController.java
+++ src/main/java/com/example/summer/trip/TripController.java
@@ -2,13 +2,36 @@ package com.example.summer.trip;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TripController {
     @RequestMapping(value="/test", method = RequestMethod.GET)
     public String test() {
         return "sehseh";
     }
+
+    /*
+    localhost:8080/product/, GET
+    */
+    @RequestMapping(value="/product", method = RequestMethod.GET)
+    public String test2() {
+        return "파라스파라 서울";
+    }
+
+    /*
+    localhost:8080/______, ___
+    pah variable로 숫자 -> 화면에 떴ㅇ면 !
+     */
+
+    @RequestMapping(value="/products/{id}", method = RequestMethod.GET)
+    public String printID(@PathVariable(value="id") int id) {
+        return id + "";
+    }
 }
+
+
+
