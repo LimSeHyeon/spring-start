81170d4fa7909e52fa501a0984ded1750a360ac9
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-18 10:37:12 +0900

    refactor: 불필요한 함수 및 주석 제거

--- src/main/java/com/example/summer/trip/TripController.java
+++ src/main/java/com/example/summer/trip/TripController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TripController {
-//    TripService tripService = new TripService();
 
     TripService tripService;
     @Autowired // 스프링아, 매개변수 '타입'에 맞는 적절한 객체 좀 넣어 줘
@@ -18,24 +17,11 @@ public class TripController {
         this.tripService = tripService;
     }
 
-    @RequestMapping(value="/test", method = RequestMethod.GET)
-    public String test() {
-        return "sehseh";
-    }
 
-    /*
-    localhost:8080/product/, GET
-    */
     @RequestMapping(value="/product", method = RequestMethod.GET)
     public String test2() {
-        String result = tripService.getProduct();
-        return result;
+        return tripService.getProduct();
     }
-
-    /*
-    localhost:8080/______, ___
-    pah variable로 숫자 -> 화면에 떴ㅇ면 !
-     */
 
     @RequestMapping(value="/products/{id}", method = RequestMethod.GET)
     public String printID(@PathVariable(value="id") int id) {
--- src/main/java/com/example/summer/trip/TripService.java
+++ src/main/java/com/example/summer/trip/TripService.java
@@ -1,11 +1,13 @@
 package com.example.summer.trip;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TripService {
     TripRepository tripRepository;
 
+    @Autowired
     TripService(TripRepository tripRepository) {
         this.tripRepository = tripRepository;
     }
