9ef4cfac169fab261ef1a108fb37c1fdbbfc5eff
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-19 15:58:34 +0900

    feat : room Class 설계대로 추가

--- src/main/java/com/example/summer/accommodation/Room.java
+++ src/main/java/com/example/summer/accommodation/Room.java
@@ -1,6 +1,36 @@
 package com.example.summer.accommodation;
 
 public class Room {
+    private int roomId;
     private String type;
     private int price;
+
+    Room(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
 }
