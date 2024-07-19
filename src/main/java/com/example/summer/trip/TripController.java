c364266a93edd47134be5c76dc9c48f66ffb3ab6
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-19 10:17:28 +0900

    refactor

--- src/main/java/com/example/summer/trip/TripController.java
+++ src/main/java/com/example/summer/trip/TripController.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.*;
 public class TripController {
 
     TripService tripService;
-    @Autowired // 스프링아, 매개변수 '타입'에 맞는 적절한 객체 좀 넣어 줘
+    @Autowired
     TripController(TripService tripService) {
         this.tripService = tripService;
     }
 
 
     @RequestMapping(value="/products/{id}", method = RequestMethod.GET)
-    public Hotel getProduct(@PathVariable(value="id") int id) {
-        return tripService.getProduct(id);
+    public Hotel getAccomodationName(@PathVariable(value="id") int id) {
+        return tripService.getAccomodationName(id);
     }
 
     @RequestMapping(value="/products", method = RequestMethod.POST)
-    public Hotel addProduct(@RequestBody Hotel hotel) {
-        return tripService.addProduct(hotel);
+    public Hotel addAccomodation(@RequestBody Hotel hotel) {
+        return tripService.addAccomodation(hotel);
     }
 }
 
--- src/main/java/com/example/summer/trip/TripService.java
+++ src/main/java/com/example/summer/trip/TripService.java
@@ -2,7 +2,6 @@ package com.example.summer.trip;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class TripService {
@@ -13,11 +12,11 @@ public class TripService {
         this.tripRepository = tripRepository;
     }
 
-    public Hotel getProduct(int id) {
+    public Hotel getAccomodationName(int id) {
         return tripRepository.findProduct(id);
     }
 
-    public Hotel addProduct(Hotel hotel) {
+    public Hotel addAccomodation(Hotel hotel) {
         return tripRepository.saveProduct(hotel);
     }
 }
