4cff6398f203790c46047a405bbc5fa164a27622
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-19 14:32:36 +0900

    feat: controller 응답 변경 -> ResponseEntity 이용

--- src/main/java/com/example/summer/accommodation/AccommodationController.java
+++ src/main/java/com/example/summer/accommodation/AccommodationController.java
@@ -1,6 +1,8 @@
 package com.example.summer.accommodation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,10 +20,15 @@ public class AccommodationController {
         return accommodationService.getAccomodationName(id);
     }
 
+    //숙소 1개 등록
     @RequestMapping(value="/products", method = RequestMethod.POST)
-    public Accommodation addAccomodation(@RequestBody Accommodation accommodation) {
-        return accommodationService.addAccomodation(accommodation);
+    public ResponseEntity<Accommodation> addAccomodation(@RequestBody Accommodation accommodation) {
+        Accommodation savedAccommodation = accommodationService.addAccomodation(accommodation);
+        return new ResponseEntity<>(savedAccommodation, HttpStatus.CREATED);
     }
+
+    //숙소 여러개 등록
+//    @RequestMapping(value = "/products", method = RequestMethod.POST)
 }
 
 
