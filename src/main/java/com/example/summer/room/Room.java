a3ccbc2296cd4955a01b4b397c8b392b0523411c
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-29 14:10:24 +0900

    feat: JPARepository 등장

--- src/main/java/com/example/summer/room/Room.java
+++ src/main/java/com/example/summer/room/Room.java
@@ -1,11 +1,14 @@
 package com.example.summer.room;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Room {
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int roomId;
     private int accommodationId;
     private String type;
---
+++ src/main/java/com/example/summer/room/RoomJPARepository.java
@@ -0,0 +1,6 @@
+package com.example.summer.room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomJPARepository extends JpaRepository<Room, Integer> {
+}
