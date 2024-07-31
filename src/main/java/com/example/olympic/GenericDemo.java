b8f532cdfe847e9282985363dff668def031183b
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-31 14:59:26 +0900

    feat : 새로운 애플리케이션 생성

---
+++ src/main/java/com/example/olympic/GenericDemo.java
@@ -0,0 +1,11 @@
+package com.example.olympic;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericDemo {
+
+    public static void main(String[] args) { SpringApplication.run(GenericDemo.class, args); }
+}
