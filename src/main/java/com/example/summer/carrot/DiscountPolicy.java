ac1c7c35916bd39c11982b85778dd4fa7b197ea1
LimSeHyeon <dlatpgus1205@naver.com>
2024-07-31 11:02:39 +0900

    feat : discountPolicy 인터페이스로 하나로 병합

---
+++ src/main/java/com/example/summer/carrot/DiscountPolicy.java
@@ -0,0 +1,27 @@
+package com.example.summer.carrot;
+
+import org.springframework.stereotype.Component;
+
+/*
+껍데기만 같던데?
+Service에서 다르게 하기
+ */
+
+public interface DiscountPolicy {
+    int getPolicy();
+}
+
+@Component
+class DiscountMemberPolicy {
+    public int getPolicy() {
+        return 1;
+    }
+}
+
+@Component
+class DiscountNotMemberPolicy {
+
+    public int getPolicy() {
+        return 0;
+    }
+}
