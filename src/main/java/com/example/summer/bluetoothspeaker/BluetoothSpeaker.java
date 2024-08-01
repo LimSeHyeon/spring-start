78a4d4f6ea0425e7d40a86a74138247d3c0665c6
LimSeHyeon <dlatpgus1205@naver.com>
2024-08-02 08:36:44 +0900

    feat : Interface 및 구현체 구현

---
+++ src/main/java/com/example/summer/bluetoothspeaker/BluetoothSpeaker.java
@@ -0,0 +1,11 @@
+package com.example.summer.bluetoothspeaker;
+
+public interface BluetoothSpeaker {
+    void connectBluetooth();
+    void disconnectBluetooth();
+    void heyGoogle();
+    void turnOn();
+    void turnOff();
+    void volumeUp();
+    void volumeDown();
+}
---
+++ src/main/java/com/example/summer/bluetoothspeaker/Mashal.java
@@ -0,0 +1,39 @@
+package com.example.summer.bluetoothspeaker;
+
+public class Mashal implements BluetoothSpeaker {
+
+    @Override
+    public void connectBluetooth() {
+        System.out.println("블루투스에 연결됩니다.");
+    }
+
+    @Override
+    public void disconnectBluetooth() {
+        System.out.println("연결을 해제합니다.");
+    }
+
+    @Override
+    public void heyGoogle() {
+        System.out.println("넹 저는 구글이에여");
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("띠리리링");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("띠로리로");
+    }
+
+    @Override
+    public void volumeUp() {
+        System.out.println("소리를 키워보자 !");
+    }
+
+    @Override
+    public void volumeDown() {
+        System.out.println("소리를 줄여보자");
+    }
+}
