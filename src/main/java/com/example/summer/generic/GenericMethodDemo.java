31574d86ded1bf92e808e04d9565b556ddfd4400
LimSeHyeon <dlatpgus1205@naver.com>
2024-08-02 11:50:50 +0900

    comment : 각 T들의 설명

--- src/main/java/com/example/summer/generic/GenericMethodDemo.java
+++ src/main/java/com/example/summer/generic/GenericMethodDemo.java
@@ -25,9 +25,9 @@ class GenericClass<C> {
 }
 
 class classWithGenericMethod<T> {
-    T t;
+    T t; // 제네릭 클래스
 
-    public <T> T genericMethod(T t1, T t2) {
+    public <T> T genericMethod(T t1, T t2) { // <T> : 나 제네릭 쓰겠다 ! 나머지 세 T 모두 같은 T !
         return t1;
     }
 }
