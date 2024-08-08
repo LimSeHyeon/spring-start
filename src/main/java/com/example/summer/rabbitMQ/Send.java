b5ec847059303feb0e0a2296c1a0fb60de70924d
LimSeHyeon <dlatpgus1205@naver.com>
2024-08-08 11:03:15 +0900

    feat: RabbitMQ 튜토리얼 1

---
+++ src/main/java/com/example/summer/rabbitMQ/Send.java
@@ -0,0 +1,24 @@
+package com.example.summer.rabbitMQ;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "빠샤빠샤";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
