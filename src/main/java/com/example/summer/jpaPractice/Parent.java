e53d7f38b0c767d528d1dce66c948d4f82ec7ff7
LimSeHyeon <dlatpgus1205@naver.com>
2024-08-12 02:04:00 +0900

    feat: 순환 참조 수정 : JsonIgnoreProperties

--- src/main/java/com/example/summer/jpaPractice/Parent.java
+++ src/main/java/com/example/summer/jpaPractice/Parent.java
@@ -1,10 +1,15 @@
 package com.example.summer.jpaPractice;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
+import org.springframework.data.repository.cdi.Eager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -12,7 +17,9 @@ public class Parent {
     @Id
     String id;
     String name;
-    ArrayList<Child> children;
+    @OneToMany(mappedBy = "parent", fetch= FetchType.EAGER)
+    @JsonIgnoreProperties({"parent"})
+    List<Child> children;
 
     public Parent(String id, String name) {
         this.id = id;
